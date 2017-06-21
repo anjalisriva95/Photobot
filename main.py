38885198fe70b26016503a023dae75b691be8128
anjalisriva95 <anjalisriva95@gmail.com>
2017-06-21 18:01:58 +0530

    A get_user_info function for fetching the details of another user

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
 from instabot import BASE_URL, ACCESS_TOKEN
-import requests
+import requests, urllib
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details"
+        print "b.Get details of a user by username"
+        print "c.Get your own recent post"
+        print "d.Get the recent post of a user by username"
+        print "e.Get a list of people who have liked the recent post of a user"
+        print "f.Like the recent post of a user"
+        print "g.Get a list of comments on the recent post of a user"
+        print "h.Make a comment on the recent post of a user"
+        print "i.Delete negative comments from the recent post of a user"
+        print "j.Exit"
+
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
+            self_info()
+        elif choice == "b":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+        #elif choice == "c":
+            #get_own_post()
+        #elif choice == "d":
+            #insta_username = raw_input("Enter the username of the user: ")
+            #get_user_post(insta_username)
+        #elif choice=="e":
+           #insta_username = raw_input("Enter the username of the user: ")
+           #get_like_list(insta_username)
+        #elif choice=="f":
+           #insta_username = raw_input("Enter the username of the user: ")
+           #like_a_post(insta_username)
+        #elif choice=="g":
+           #insta_username = raw_input("Enter the username of the user: ")
+           #get_comment_list(insta_username)
+        #elif choice=="h":
+           #insta_username = raw_input("Enter the username of the user: ")
+           #make_a_comment(insta_username)
+        #elif choice=="i":
+           #insta_username = raw_input("Enter the username of the user: ")
+           #delete_negative_comment(insta_username)
+        #elif choice == "j":
+            #exit()
+        else:
+            print "wrong choice"
+            exit()
+
+#FUNCTION DECLARATION FOR SELF INFO OF THE USER.
 
 def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (ACCESS_TOKEN)
@@ -16,4 +68,42 @@ def self_info():
     else:
         print 'Status code other than 200 received!'
 
-self_info()
+#FUNCTION DECLARATION TO GET THE USER ID FROM USER NAME
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+#FUNCTION DECLARATION TO GET THE INFO OF THE USER FROM THE USERNAME
+
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+
+start_bot()
